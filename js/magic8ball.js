18783cb3c64b78389a502e4789bba4ab51cacd1f
RenCS99 <rcuevasalas99@gmail.com>
2022-10-12 01:19:19 -0700

    Added an array with img names and for loop

--- js/magic8ball.js
+++ js/magic8ball.js
@@ -1,7 +1,11 @@
 let imgArr = [];
+let imgOptions = ["yes-ball.png", "no-ball.png", "itwillpass-ball.png", "askagain-ball.png", "absolutely-ball.png", "cannottell-ball.png", "countonit-ball.png", "itisok-ball.png", "maybe-ball.png", "nodoubt-ball.png", "verylikely-ball.png", "waitforit-ball.png", "yourehot-ball.png", "notnow-ball.png"];
 
-imgArr[0] = new Image();
-imgArr[0].src = '../img/yes-ball.png';
+for (let i = 0; i < imgOptions.length; i++) {
+    imgArr[i] = new Image();
+    imgArr[i].src = 'img/' + imgOptions[i];
+    console.log(imgArr[i]);
+}
 
 function askAnything() {
     let response = document.getElementById("response");
